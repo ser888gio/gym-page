0f9b378fe3fc5b9eba0cab4692189124232988e9
Sergio <104572860+ser888gio@users.noreply.github.com>
2024-07-03 12:22:11 +0200

    Fix for an issue for hosting on GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "https://ser888gio.github.io/gym-page/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
